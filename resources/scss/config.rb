8ddc118834ffdd42ab046f739d2b3549443bbc17
Scott <buchanan.sc@gmail.com>
2012-02-14 02:18:10 -0800

    project is now configured (and using) compass (http://compass-style.org)

---
+++ resources/scss/config.rb
@@ -0,0 +1,10 @@
+sass_path = File.dirname(__FILE__)
+css_path = File.join(sass_path, "..", "css")
+images_dir = "../images"
+
+relative_assets = true
+
+# Specify the output style/environment
+# output_style = :compressed
+output_style = :expanded
+environment = :production
