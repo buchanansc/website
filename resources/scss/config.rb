2a5216380de504b10af7b3229ca21f7bd67512cf
Scott Buchanan <buchanan.sc@gmail.com>
2012-03-14 02:17:34 -0700

    * I guess it's beta now. It's to a point where we're presentable (if not yet browser compliant). * Going to cut of the design here for now, and focus on testing. After that, fill it with some content.

--- resources/scss/config.rb
+++ resources/scss/config.rb
@@ -2,9 +2,11 @@ sass_path = File.dirname(__FILE__)
 css_dir = "../css"
 javascripts_dir = "../../lib"
 images_dir = "../images"
+# images_path = "/resources/images"
 
 relative_assets = true
 
 # environment = :production
 # environment = :development
 output_style = (environment == :production) ? :compressed : :expanded
+
